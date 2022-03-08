5d3ba5714b7ad13a4bf0db7e6793a047f9d0d01c
italo-baciliere <italoluciano57@gmail.com>
2022-03-08 14:49:17 -0300

    02. Lista estáticas: Tuplas

---
+++ Caderno.py
@@ -0,0 +1,24 @@
+'''
+FAÇA ESSE CURSO DE DATA SCIENCE E:
+
+    * Saiba como funcionam tuplas e listas
+    * Conheça a biblioteca dos cientistas de dados: Pandas
+    * Encapsule código em funções
+    * Use pacotes para organizar o código
+    * Trabalhe com Built-in Functions do Python
+
+AULAS
+
+    * 01. Apresentação do curso
+        - Os ambientes de desenvolvimento para a linguagem Python
+        - A criar DataFrames com o pacote pandas, a partir de dados externos
+        - Como obter informações básicas de um DataFrame
+        - Como obter estatísticas descritivas dos dados de um DataFrame
+    * 02. Listas estáticas: Tuplas
+        - O qe são as tuplas
+        - Formas de criação de uma tupla
+        - Técnicas de seleção de itens e fatiamento com tuplas do Python
+        - Formas de iterar por uma tupla
+        - A técnica conhecida como desempacotamento de tuplas
+        - A utilizar a built-in function zip()
+'''
